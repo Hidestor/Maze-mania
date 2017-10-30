a073f00264c53b54064e2e6e766001a1c5d3ea76
Hidestor <30600908+Hidestor@users.noreply.github.com>
2017-10-30 09:50:27 +0530

    Update Depth_first_search.py

--- Depth_first_search.py
+++ Depth_first_search.py
@@ -1,28 +1,27 @@
+# imported deque from collections module
 from collections import deque
 
 def solve(maze):
     start = maze.start
     end = maze.end
     width = maze.width
-    stack = deque([start])
+    stack = deque([start]) # stack is used for DFS
     shape = (maze.height, maze.width)
-    prev = [None] * (maze.width * maze.height)
-    visited = [False] * (maze.width * maze.height)
+    prev = [None] * (maze.width * maze.height) # initialize prev as None
+    visited = [False] * (maze.width * maze.height) # mark visited as false for all the nodes initially
     count = 0
 
-    completed = False
+    completed = False 
+    
     while stack:
         count += 1
         current = stack.pop()
 
-        if current == end:
+        if current == end: # if current is the destination node then stop here only.
             completed = True
             break
 
-        visited[current.Position[0] * width + current.Position[1]] = True
-
-        #import code
-        #code.interact(local=locals())
+        visited[current.Position[0] * width + current.Position[1]] = True 
 
         for n in current.Neighbours:
             if n != None:
@@ -33,7 +32,7 @@ def solve(maze):
 
     path = deque()
     current = end
-    while (current != None):
+    while (current != None): #retracing the path from destination to start point
         path.appendleft(current)
         current = prev[current.Position[0] * width + current.Position[1]]
 
