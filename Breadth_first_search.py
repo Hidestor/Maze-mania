ad5f26946faf877e1f08aa777934ef7236dd28b5
Hidestor <30600908+Hidestor@users.noreply.github.com>
2017-10-30 09:45:28 +0530

    Update Breadth_first_search.py

--- Breadth_first_search.py
+++ Breadth_first_search.py
@@ -1,41 +1,42 @@
+# Deque importes from collection module
 from collections import deque
 
 def solve(maze):
-    start = maze.start
-    end = maze.end
+    start = maze.start #starting coordinated of the maze
+    end = maze.end #End coordinates of the maze
 
     width = maze.width
 
     queue = deque([start])
     shape = (maze.height, maze.width)
-    prev = [None] * (maze.width * maze.height)
-    visited = [False] * (maze.width * maze.height)
+    prev = [None] * (maze.width * maze.height) #To store the parent of the node for generating path 
+    visited = [False] * (maze.width * maze.height) #To store the nodes visited
 
     count = 0
 
-    completed = False
+    completed = False # Flag to know end point reached or not
 
-    visited[start.Position[0] * width + start.Position[1]] = True
+    visited[start.Position[0] * width + start.Position[1]] = True # mark source as visited
 
     while queue:
         count += 1
-        current = queue.pop()
+        current = queue.pop() 
 
-        if current == end:
+        if current == end: #if current is the destination node then stop here only.
             completed = True
             break
 
         for n in current.Neighbours:
             if n != None:
                 npos = n.Position[0] * width + n.Position[1]
-                if visited[npos] == False:
+                if visited[npos] == False: #if node n is not visited yet then put it into the queue
                     queue.appendleft(n)
-                    visited[npos] = True
-                    prev[npos] = current
+                    visited[npos] = True # mark it as visited
+                    prev[npos] = current # store its parent as current
 
     path = deque()
     current = end
-    while (current != None):
+    while (current != None): #tracing the path from destination to start
         path.appendleft(current)
         current = prev[current.Position[0] * width + current.Position[1]]
 
