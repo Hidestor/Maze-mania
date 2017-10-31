cc2dfadb371af7759d18867dd411bd4a33e6f61c
Hidestor <30600908+Hidestor@users.noreply.github.com>
2017-10-31 17:53:23 +0530

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
 from bprofile import BProfile
 
+# This module generates temporary files and directories
 import tempfile
 from solve import solve
 from factory import SolverFactory
@@ -27,6 +27,16 @@ inputs = [
     # "perfect10k",
     # "vertical15k",
 ]
+
+# NamedTemporaryFile:
+# This function operates exactly as TemporaryFile() does, except that the file is guaranteed to have a visible name in the file system 
+# (on Unix, the directory entry is not unlinked). That name can be retrieved from the name attribute of the returned file-like object.
+# Whether the name can be used to open the file a second time, while the named temporary file is still open, varies across platforms 
+# (it can be so used on Unix; it cannot on Windows NT or later). If delete is true (the default), the file is deleted as soon as 
+# it is closed.
+
+# The returned object is always a file-like object whose file attribute is the underlying true file object. This file-like object
+# can be used in a with statement, just like a normal file.
 
 def profile():
     for m in methods:
